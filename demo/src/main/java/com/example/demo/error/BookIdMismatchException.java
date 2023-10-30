f3304e1f55371d3edf18576a9adf4c85d54ceefe
Camila Y. Stock <104857326+camilastock-tarmac@users.noreply.github.com>
2023-10-30 16:59:55 -0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/error/BookIdMismatchException.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.error;
-
-public class BookIdMismatchException extends Throwable {
-}
--- demo/src/main/java/com/example/demo/error/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.error;
-
-public class BookNotFoundException extends RuntimeException {
-
-    public BookNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
